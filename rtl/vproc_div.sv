42f0b122b749e7056362499482fe844ef714a20b
Sebastian Emanuel Dawid <sdawid@techfak.uni-bielefeld.de>
2026-09-11 09:49:50 +0200

    Fix bit count in op_unit and padding size in op_mode_bf16

--- rtl/vproc_div.sv
+++ rtl/vproc_div.sv
@@ -279,7 +279,9 @@ module vproc_div #(
             .result_o           ( div_out[32*g +: 32]           ),
 
             // divider enable, not affected by kill/halt
-            .div_en_i           ( 1'b1                          ), //
+`ifdef CV32E40X_DIV_HAS_ENABLE
+            .div_en_i           ( 1'b1                          ),
+`endif
 
             // Handshakes
             .valid_i            ( data_valid_i_q                        ), //comes from EX_VALID
